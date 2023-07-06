7e9602f86991052ef4663397f9bc7a871a71cf95
ohjiyang <ohjiyung@icloud.com>
2023-07-07 01:20:07 +0600

    feat: initial styling, created ThemeProvider, ThemeContext, useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import './index.scss';
+import {useTheme} from "./theme/useTheme";
+import './styles/index.scss';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>theme switcher</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
