49e128aa30989de1f94b017e2a4cfad8c1b6bc7e
ohjiyang <ohjiyung@icloud.com>
2023-07-07 01:10:43 +0600

    feat: installed react-router-dom, created AboutPage, MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import './index.scss';
 
 const App = () => {
     return (
         <div className="app">
-            hello world
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
