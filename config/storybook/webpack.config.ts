9fd30396668cb106058cc3ba5ac17b106e570dbd
ohjiyang <ohjiyung@icloud.com>
2023-07-09 21:41:07 +0600

    feat: installed loki

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -16,6 +16,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
+    // eslint-disable-next-line no-param-reassign
     config.module?.rules = config.module?.rules?.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return {
