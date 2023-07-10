eb9fa942c32555221d13925943c099ba79a98f79
ohjiyang <ohjiyung@icloud.com>
2023-07-10 21:50:58 +0600

    fix: created componentRouter, fixing tests with router

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import {
-    renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { ComponentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
     test('Sidebar test render', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Sidebar test 2', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         const toggleButton = screen.getByTestId('sidebar-toggle');
         fireEvent.click(toggleButton);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
