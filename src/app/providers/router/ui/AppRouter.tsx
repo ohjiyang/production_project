072af012b24cca627a5d191cbd6a14d54df047a7
ohjiyang <ohjiyung@icloud.com>
2023-07-10 21:41:09 +0600

    fix: style fixing

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,15 @@ const AppRouter = () => (
     <Suspense fallback={<PageLoader />}>
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
-                <Route key={path} path={path} element={element} />
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    )}
+                />
             ))}
         </Routes>
     </Suspense>
