611b4d7332d1cfb27f7883f667233bf7f4f2814a
ohjiyang <ohjiyung@icloud.com>
2023-07-12 15:20:59 +0600

    feat: created ui kit Modal, Portal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,11 +12,25 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
 
+    const [isAuthModalOpen, setAuthModalOpen] = useState(false);
+
+    const handleOnToggleModal = useCallback(() => {
+        setAuthModalOpen((prevState) => !prevState);
+    }, []);
+
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ThemeButton.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={handleOnToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModalOpen} onClose={handleOnToggleModal}>
+                Hello mir
+            </Modal>
         </div>
     );
 };
